91f11e794eb3f0a701f66bfe3e1c0a3cc52c6c37
koheinishimura66 <t13551kn@sfc.keio.ac.jp>
2014-05-02 12:16:36 +0900

    提出用（西村光平）

--- js/slidein.js
+++ js/slidein.js
@@ -1,14 +1,14 @@
-var endSlideinFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.setAttribute("class", "");
+var endSlideinFoxkeh = function(){//10行目が実行されたときに開始される
+	var foxkeh = document.querySelector("#slidein-foxkeh img");//７行目と同じ
+	foxkeh.setAttribute("class", "");//classを空白に変更（最初の状態に戻る）
 };
 
-var startSlideinFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.setAttribute("class", "slidein");
+var startSlideinFoxkeh = function(){//14行目が実行されると開始する
+	var foxkeh = document.querySelector("#slidein-foxkeh img");//slidein-foxkeh直下のimgをfoxkehと名付ける
+	foxkeh.setAttribute("class", "slidein");//classをslideinにっする（cssの.slideinに書かれているアニメーションが実行される
 
-	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
+	foxkeh.addEventListener("animationend", endSlideinFoxkeh);//アニメーションが終わった時endSlideinFoxkehを走らせる
 };
 
-var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");slidein-foxkeh直下のbuttonを
-slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");//slidein-foxkeh直下のbuttonをslideinFoxkehButtonと名付ける
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);//clickしたときにstartSlideinFoxkehを開始
